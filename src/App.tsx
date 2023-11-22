2e2c756060399e972caabc82a0923b00ef1bc4ea
Van Tran <osd_van.tran@nfcguest.com>
2023-11-22 21:55:12 +0700

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more. Testing for pr to develop branch
+        Click on the Vite and React logos to learn more. Testing for pr rules
       </p>
     </>
   )
